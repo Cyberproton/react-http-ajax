c8defd80a6766ced10ee8d31adcdb56339e97578
Cyberproton <cyberprotonatmailbox@gmail.com>
2020-07-25 21:57:16 +0700

    Add selected post feature

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        loadedPost: null
+    };
+
+    // setState() should be handled carefully or it may cause infinite update loop
+    componentDidUpdate() {
+        // If no post is being selected
+        if (!this.props.id) {
+            return;
+        }
+        // If the post has been loaded and the loaded post stay the same
+        if (this.state.loadedPost && this.state.loadedPost.id === this.props.id) {
+            return;
+        }
+        Axios
+            .get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                this.setState({ loadedPost: response.data });
+            });
+    }
+
+    render () {
+        let post = null;
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
+        }
         return post;
     }
+
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,25 +8,43 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state = {
+        posts: [],
+        selectedPostId: null
+    };
+
     // Side-effects go here, this function does not trigger re-render
     componentDidMount() {
         Axios
             .get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                const posts = response.data
+                    .slice(0, 4)
+                    .map(post => ({
+                        ...post,
+                        author: 'Cyberproton'
+                    }));
+                this.setState({ posts: posts });
             });
     }
 
     render() {
+        const posts = this.state.posts.map(post => 
+            <Post key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={this.postSelectedHandler.bind(this, post.id)}
+            />
+        );
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost 
+                        id={this.state.selectedPostId} 
+                    />
                 </section>
                 <section>
                     <NewPost />
@@ -34,6 +52,10 @@ class Blog extends Component {
             </div>
         );
     }
+
+    postSelectedHandler = (postId) => {
+        this.setState({ selectedPostId: postId });
+    };
 
 }
 
