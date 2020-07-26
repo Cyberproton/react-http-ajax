aa5d452ddd06499591fa4172f0f26046436db79f
Cyberproton <cyberprotonatmailbox@gmail.com>
2020-07-26 20:16:29 +0700

    Add request config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Axios from 'axios';
+
+// Edit request config here
+Axios.interceptors.request.use(request => {
+  console.log(request);
+  return request;
+});
 
 ReactDOM.render(
   <React.StrictMode>
